3d79621f7ff0f59c0458723440e777000ce54c42
Benjamin <lenjaminbiu@gmail.com>
2020-09-21 15:53:55 +1000

    first change

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render( <
+  h1 > Hello World < /h1>,
   document.getElementById('root')
 );
 
